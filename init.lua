561985a16bb88e367a7a1c71aa28455b60b668e4
farfadet46 <farfadet46@gmail.com>
2016-03-21 16:38:02 +0100

    ajout mitraillette
    + texture boue

--- init.lua
+++ init.lua
@@ -5,8 +5,48 @@ minetest.register_node("tranchee:sac_sable",{
 	inventory_image = "inv_sac_sable.png",
 	wield_image = "inv_sac_sable.png",
 	paramtype="light",
-	walkable = false,
+	--walkable = false,
 	--drop="recycle:bouteille_eau",
 	--buildable_to = false,
 	groups = {crumbly=2},
+})
+
+minetest.register_node("tranchee:boue",{
+	description = "Boue",
+	drawtype="node",
+	tiles ={"boue.png"},
+	--inventory_image = "boue.png",
+	--wield_image = "inv_sac_sable.png",
+	paramtype="light",
+	--walkable = false,
+	--drop="recycle:bouteille_eau",
+	--buildable_to = false,
+	groups = {crumbly=2},
+})
+
+minetest.register_node("tranchee:boue1",{
+	description = "Boue1",
+	drawtype="node",
+	tiles ={"boue1.png"},
+	--inventory_image = "boue.png",
+	--wield_image = "inv_sac_sable.png",
+	paramtype="light",
+	--walkable = false,
+	--drop="recycle:bouteille_eau",
+	--buildable_to = false,
+	groups = {crumbly=2},
+})
+
+minetest.register_node("tranchee:mitraillette",{
+	description = "Mitraillette fixe",
+	drawtype = "mesh",
+	mesh = "mitraillette.b3d",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	tiles = {"mitrailette.png"},
+	inventory_image = "img_mitraillette.png",
+	groups = {cracky=1},
+	--on_rightclick = function(pos, node, player, pointed_thing)
+		
+	--end,
 })
